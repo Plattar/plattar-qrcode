f5ad35c8d1082ab5afd727f0e57ce8e46d03defc
David Arayan <david.arayan12@gmail.com>
2021-08-13 13:29:24 +1000

    - Remove manual publish.sh - Added Github NPM publish workflow - Added Version which gets updated automatically - Added package clean:build proc for github actions

--- plattar-qrcode/index.js
+++ plattar-qrcode/index.js
@@ -1,6 +1,11 @@
 "use strict";
 const QRCodeElement = require("./elements/qrcode-element.js");
+const Version = require("./version");
+
+console.log("using @plattar/plattar-qrcode v" + Version);
 
 customElements.define("plattar-qrcode", QRCodeElement);
 
-module.exports = {};
+module.exports = {
+    Version: Version
+};
--- publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf plattar-qrcode/README.md plattar-qrcode/graphics plattar-qrcode/node_modules plattar-qrcode/build plattar-qrcode/package-lock.json
-cp README.md plattar-qrcode/README.md
-cp -R graphics plattar-qrcode/
-cd plattar-qrcode && npm run build && npm publish --scope=public && cd ../
-rm -rf plattar-qrcode/README.md plattar-qrcode/graphics plattar-qrcode/node_modules plattar-qrcode/build plattar-qrcode/package-lock.json
