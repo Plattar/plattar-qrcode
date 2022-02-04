26d6f600ec7e4605df4604fddf3d6bc41faf2dc8
David Arayan <david.arayan12@gmail.com>
2022-02-04 15:16:20 +1100

    - Removed package-lock.json from repository, was not needed - Ensure plattar-qrcode node is not added as a custom element multiple times (bug) - Updated packages

--- plattar-qrcode/index.js
+++ plattar-qrcode/index.js
@@ -2,9 +2,13 @@
 const QRCodeElement = require("./elements/qrcode-element.js");
 const Version = require("./version");
 
-console.log("using @plattar/plattar-qrcode v" + Version);
+if (customElements) {
+    if (customElements.get("plattar-qrcode") === undefined) {
+        customElements.define("plattar-qrcode", QRCodeElement);
+    }
+}
 
-customElements.define("plattar-qrcode", QRCodeElement);
+console.log("using @plattar/plattar-qrcode v" + Version);
 
 module.exports = {
     version: Version
